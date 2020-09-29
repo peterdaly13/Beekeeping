f1202bcf5d838e0789ac882200e722c6a5032832
satres <trasmussen3@wisc.edu>
2020-09-29 18:18:46 -0500

    User fields and functions

--- app/src/main/java/com/example/beekeeping/User.java
+++ app/src/main/java/com/example/beekeeping/User.java
@@ -1,4 +1,35 @@
 package com.example.beekeeping;
 
+import android.media.Image;
+
+// Each beekeeper is represented as a user
 public class User {
+    int id;
+    String name;
+    String pswd; // look into other classes for password handling
+    Image profilePic;
+    Apiary apiary;
+    String phone;
+    String email;
+
+    User(String name, String pswd) {
+        this.name = name;
+        this.pswd = pswd;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getId() {
+        return this.id;
+    }
+
+    void deleteUser() {
+        // how do we want to go about deleting users?
+    }
 }
