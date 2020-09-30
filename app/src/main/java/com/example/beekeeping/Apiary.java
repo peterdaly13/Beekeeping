f668821a258a3dd786d9b14ab33df8582ae5eb3a
yuanchenGH <71296283+yuanchenGH@users.noreply.github.com>
2020-09-30 16:27:17 -0500

    First version

--- app/src/main/java/com/example/beekeeping/Apiary.java
+++ app/src/main/java/com/example/beekeeping/Apiary.java
@@ -7,31 +7,28 @@ public class Apiary {
         public int aid;
         public List<Hive> hives;
 
-        public Apiary(int aid) {
+        public Apiary() {
             hives = new ArrayList<Hive>();
             generateAID();
         }
 
-        public Apiary() {
-        }
+        public int getAid() { return aid; }
 
-        public int getAid() {
-            return aid;
-        }
+        public void setAid(int aid) { this.aid = aid; }
 
-        public void setAid(int aid) {
-            this.aid = aid;
-        }
+        public List<Hive> getHives() { return hives; }
 
-        public List<Hive> getHives() {
-            return hives;
-        }
+        public void setHives(ArrayList<Hive> hives) { this.hives = hives; }
 
-        public void setHives(ArrayList<Hive> hives) {
-            this.hives = hives;
+        public String toString(){
+            //String info = "Apiary #" + aid + "manages the following hives: \n";
+            String info = "Apiary #" + aid + "manages " + hives.size() + " hives.";
+            return info;
         }
 
         private void generateAID() {
             this.aid = 0; //TODO
         }
-}
+
+
+    }
