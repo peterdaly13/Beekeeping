67491f3747b436ec24233670b41e3ae277bc3800
peterdaly13 <pndaly@wisc.edu>
2020-10-02 22:00:02 -0500

    Hives listview cont

--- app/src/main/java/com/example/beekeeping/HiveActivity.java
+++ app/src/main/java/com/example/beekeeping/HiveActivity.java
@@ -2,7 +2,11 @@ package com.example.beekeeping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HiveActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class HiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hive);
+
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        Intent intent = getIntent();
+        final String hid = intent.getStringExtra("hid");
+
+
+
+
     }
 }
